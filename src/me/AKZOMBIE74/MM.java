3c47d9896d21d969192f125ebff38b5e2b7bd0e8
BuildTools <monkey62016@hotmail.com>
2017-06-28 15:59:38 -0400

    Optimization stuff

--- src/me/AKZOMBIE74/MM.java
+++ src/me/AKZOMBIE74/MM.java
@@ -12,9 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 
 /**
@@ -27,6 +30,10 @@ public class MM extends JavaPlugin {
 
     private MapData data;
 
+    //Update stuff
+    String VERSION, CURRENT_VERSION, CHANGELOG;
+    Boolean shouldUpdate;
+
     @Override
     public void onEnable(){
         instance = this;
@@ -36,7 +43,7 @@ public class MM extends JavaPlugin {
         pm.registerEvents(new ChangeIt(), instance);
         pm.registerEvents(new MapListener(), instance);
         pm.registerEvents(new leave(), instance);
-        pm.registerEvents(new preProcess(), instance);
+        pm.registerEvents(new WorldChanged(), instance);
 
         getCommand("mm").setExecutor(new MyMap());
 
@@ -44,9 +51,7 @@ public class MM extends JavaPlugin {
 
         FACE_SIZE=getConfig().getInt("faceSize");
         scale = MapView.Scale.valueOf(getConfig().getString("scale"));
-        getLogger().info("Test commit");
-
-
+        checkForUpdates();
         getLogger().info("Enabled!");
     }
 
@@ -100,15 +105,99 @@ public class MM extends JavaPlugin {
                 saveDefaultConfig();
                 getConfig().set("faceSize", 4);
                 getConfig().set("scale", "CLOSEST");
+                getConfig().set("show-update-message", true);
                 saveConfig();
             } else {
                 getLogger().info("Config.yml found, loading!");
+                checkForValues();
+                getLogger().info("Config.yml loaded!");
             }
         } catch (Exception e) {
             e.printStackTrace();
 
         }
 
+    }
+
+    private void checkForValues(){
+        if (!(getConfig().isInt("faceSize")
+                && getConfig().isString("scale")
+                && getConfig().isBoolean("show-update-message"))) {
+            if (!getConfig().isInt("faceSize")) {
+                getConfig().set("faceSize", 4);
+            }
+            if (!getConfig().isString("scale")) {
+                getConfig().set("scale", "CLOSEST");
+            }
+            if (!getConfig().isBoolean("show-update-message")) {
+                getConfig().set("show-update-message", true);
+            }
+            saveConfig();
+        }
+    }
+
+    private String connectToVersion(String server){
+        URL uri;
+        try {
+            uri = new URL(server);
+            URLConnection ec = uri.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    ec.getInputStream(), "UTF-8"));
+            String inputLine;
+            StringBuilder a = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                a.append(inputLine);
+
+            in.close();
+            return a.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * Compares two version strings.
+     *
+     * Use this instead of String.compareTo() for a non-lexicographical
+     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+     *
+     * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
+     *
+     * @param str1 a string of ordinal numbers separated by decimal points.
+     * @param str2 a string of ordinal numbers separated by decimal points.
+     * @return The result is a negative integer if str1 is _numerically_ less than str2.
+     *         The result is a positive integer if str1 is _numerically_ greater than str2.
+     *         The result is zero if the strings are _numerically_ equal.
+     */
+    public static int versionCompare(String str1, String str2) {
+        String[] vals1 = str1.split("\\.");
+        String[] vals2 = str2.split("\\.");
+        int i = 0;
+        // set index to first non-equal ordinal or length of shortest version string
+        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+            i++;
+        }
+        // compare first non-equal ordinal number
+        if (i < vals1.length && i < vals2.length) {
+            int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+            return Integer.signum(diff);
+        }
+        // the strings are equal or one string is a substring of the other
+        // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+        return Integer.signum(vals1.length - vals2.length);
+    }
+    private void checkForUpdates(){
+        CURRENT_VERSION = getInstance().getDescription().getVersion();
+        if (getConfig().getBoolean("show-update-message")) {
+            String VersionAndChangelog = connectToVersion(
+                    "https://private-8f513b-myspigotpluginupdates.apiary-mock.com/questions").split(",")[0]
+                    .replaceAll("\"", ""); //MaraudersMap:Version:Changelog
+            VERSION = VersionAndChangelog.split(":")[1]
+                    .replaceAll(" ", "");//Version
+            CHANGELOG = VersionAndChangelog.split(":")[2];//Changelog
+            //Set boolean variable
+            shouldUpdate = versionCompare(CURRENT_VERSION, VERSION) < 0;
+        }
     }
 
 
--- src/me/AKZOMBIE74/MyMap.java
+++ src/me/AKZOMBIE74/MyMap.java
@@ -1,10 +1,13 @@
 package me.AKZOMBIE74;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.map.MapView;
 
 /**
  * Created by Amin on 3/25/2017.
@@ -18,7 +21,10 @@ public class MyMap implements CommandExecutor {
             Player p = (Player) sender;
                 if (p.hasPermission("marauders.use")) {
                     if (cmd.getName().equalsIgnoreCase("mm")) {
-
+                        ItemStack is = MM.getInstance().createMap();
+                        MapView mapView = Bukkit.getMap(is.getDurability());
+                        myRender.applyToMap(mapView);
+                        p.getInventory().addItem(is);
                         p.sendMessage(ChatColor.GREEN + "Received Marauders Map");
                     }
                 }
