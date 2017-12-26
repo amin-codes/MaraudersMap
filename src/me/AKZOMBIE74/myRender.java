50b87c2377f4a624f7b5d571fbc7431dd5b8aff1
AKZOMBIE74 <6474139+AKZOMBIE74@users.noreply.github.com>
2017-12-26 09:26:50 -0500

    Update myRender.java

--- src/me/AKZOMBIE74/myRender.java
+++ src/me/AKZOMBIE74/myRender.java
@@ -1,13 +1,37 @@
 package me.AKZOMBIE74;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.map.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 
 /**
  * Created by Amin on 3/25/2017.
  */
 public class myRender extends MapRenderer{
+    private static String server_version = MM.getInstance().getServerVersion();
+    private static final String craft_maprenderer = "org.bukkit.craftbukkit." + server_version + ".map.CraftMapRenderer";
+    private static final String craft_map_view = "org.bukkit.craftbukkit." + server_version + ".map.CraftMapView";
+    private static final String world_map = "net.minecraft.server." + server_version + ".WorldMap";
+    private static final String minecraft_server = "net.minecraft.server." + server_version + ".MinecraftServer";
+    private static final String world_server = "net.minecraft.server." + server_version + ".WorldServer";
+    private static final String persistant_collection ="net.minecraft.server." + server_version + ".PersistentCollection";
+
+
+    private static final Class<?> CraftMapRender = getClass(craft_maprenderer);
+    private static final Class<?> CraftMapV = getClass(craft_map_view);
+    private static final Class<?> WorldM = getClass(world_map);
+    private static final Class<?> MinecraftS = getClass(minecraft_server);
+    private static final Class<?> WorldS = getClass(world_server);
+    private static final Class<?> PersistantC = getClass(persistant_collection);
+
 
     myRender(){
         super(true);
@@ -15,13 +39,48 @@ public class myRender extends MapRenderer{
 
     @Override
     public void render(MapView mapView, MapCanvas mapCanvas, Player player) {
-        mapView.setScale(MM.getInstance().scale());
-        mapView.setWorld(player.getWorld());
-        Integer playerX = (int)player.getLocation().getBlockX();
-        Integer playerZ = (int)player.getLocation().getBlockZ();
 
-        Integer realImageSize = MM.getInstance().imageSize();
-        Integer imageSize = realImageSize/2;
+        MapView.Scale scale = MM.getInstance().scale();
+        try {
+            Method getServer = MinecraftS.getMethod("getServer");
+            Object s = getServer.invoke(null); //Returns MinecraftServer
+            Field worlds_1 = s.getClass().getField("worlds");
+            worlds_1.setAccessible(true);
+            List worlds_final = (List) worlds_1.get(s);
+            Object worlds = WorldS.cast(worlds_final.get(0));
+
+            Field world_maps = worlds.getClass().getField("worldMaps");
+            Object world_maps_final = PersistantC.cast(world_maps.get(worlds));
+            Method w = world_maps_final.getClass().getMethod("get", PersistantC.getClass(), String.class);
+
+
+            Constructor craftMapConstructor = CraftMapRender.getDeclaredConstructor(CraftMapV, WorldM);
+            craftMapConstructor.setAccessible(true);
+            Object world_map = WorldM.cast(w.invoke(world_maps_final, WorldM.getClass(), "map_"+mapView.getId()));
+            Object craftMapRenderer = craftMapConstructor.newInstance(CraftMapV.cast(mapView),
+                    world_map);
+
+
+            /*CraftMapRenderer craftMapRenderer1 = new CraftMapRenderer((CraftMapView) mapView,
+                    (WorldMap) MinecraftServer.getServer().worlds.get(0).worldMaps.get(WorldMap.class, "map_" + mapView.getId()));*/
+            craftMapRenderer.getClass().getMethod("initialize", MapView.class).invoke(craftMapRenderer, mapView);
+            //craftMapRenderer.initialize(mapView);
+            craftMapRenderer.getClass().getMethod("render", MapView.class, MapCanvas.class, Player.class).invoke(craftMapRenderer, mapView, mapCanvas, player);
+            //craftMapRenderer.render(mapView, mapCanvas, player);
+
+        } catch (NoSuchMethodException |  IllegalAccessException | NoSuchFieldException | InstantiationException | InvocationTargetException exception) {
+            //Do nothing
+            exception.printStackTrace();
+        }
+
+        int playerX = player.getLocation().getBlockX();
+        int playerZ = player.getLocation().getBlockZ();
+
+        int realImageSize = 4;
+        try {
+            realImageSize = MM.getInstance().imageSize();
+        } catch (NullPointerException e) {}
+        int imageSize = realImageSize/2;
 
         if (playerX != 0) {
             if (playerZ != 0) {
@@ -40,18 +99,21 @@ public class myRender extends MapRenderer{
             }
         }
 
+        int centerX = mapView.getCenterX();
+        int centerZ = mapView.getCenterZ();
 
-        Integer centerX = mapView.getCenterX();
-        Integer centerZ = mapView.getCenterZ();
-        MM.getInstance().getData().getImages().forEach((key, image) -> {
-            if (key.getWorld().equals(player.getWorld())) {
-                Integer distanceX = (int) (63.5 * key.getLocation().getBlockX())/centerX;
-                Integer distanceZ = (int) (63.5 * key.getLocation().getBlockZ())/centerZ;
-                Integer totalDistance = (int) key.getLocation().distance(player.getLocation());
+        MM.getInstance()
+                .getData()
+                .getImages()
+                .entrySet()
+                .stream()
+                .filter(map -> Bukkit.getServer().getPlayer(map.getKey()).getWorld().equals(player.getWorld()) && Bukkit.getServer().getPlayer(map.getKey()).isOnline() && isInRange(Bukkit.getServer().getPlayer(map.getKey()).getLocation(), player.getLocation(), scale)).forEach(set -> { //key = player uuid, value = image
+                Player key = Bukkit.getServer().getPlayer(set.getKey());
+                //int distanceX = (int) (63.5 * key.getLocation().getBlockX())/centerX;
+                //int distanceZ = (int) (63.5 * key.getLocation().getBlockZ())/centerZ;
+                int[] coords = convertLocationToMapCoords(centerX, centerZ, key.getLocation(), scale);
 
-
-                mapCanvas.drawImage((int) ((distanceX - imageSize)), (int) ((distanceZ - imageSize)), image);
-            }
+                mapCanvas.drawImage(coords[0] - imageSize, coords[1] - imageSize, set.getValue());
         });
     }
 
@@ -60,13 +122,58 @@ public class myRender extends MapRenderer{
         super.initialize(map);
     }
 
-    static void applyToMap(MapView map) {
+    private static int getScaleSize(MapView.Scale scale ) {
+        if ( scale.equals( MapView.Scale.CLOSEST ) ) return 1;
+        else if ( scale.equals( MapView.Scale.CLOSE ) ) return 2;
+        else if ( scale.equals( MapView.Scale.NORMAL ) ) return 4;
+        else if ( scale.equals( MapView.Scale.FAR ) ) return 8;
+        else if ( scale.equals( MapView.Scale.FARTHEST ) ) return 16;
+        return 0;
+    }
+
+    //returns [x, y]
+    private static int[] convertLocationToMapCoords(int centerX, int centerZ, Location l1, MapView.Scale scale)
+    {
+        int scale_size = getScaleSize(scale);
+        int distanceX = ((l1.getBlockX() - centerX) / scale_size) + 63;
+        int distanceZ = ((l1.getBlockZ() - centerZ) / scale_size) + 63;
+        return new int[]{distanceX, distanceZ};
+    }
+
+    private static boolean isInRange(Location p1, Location p2, MapView.Scale scale)
+    {
+        int scale_size = getScaleSize(scale) * 128;
+        return p1.distance(p2) <= scale_size;
+    }
+
+
+
+    static void applyToMap(MapView map, MapView.Scale... scale) {
         if(map != null){
+            if (scale.length > 0) map.setScale(scale[0]);
+            else map.setScale(MM.getInstance().scale());
             for (MapRenderer renderer : map.getRenderers()) {
                 map.removeRenderer(renderer);
+
             }
-            map.addRenderer(MM.getInstance().getData().getDefaultMap());
             map.addRenderer(new myRender());
+            //map.addRenderer(MM.getInstance().getData().getDefaultMap());
+            //map.addRenderer();
         }
+    }
+
+    private static Class<?> getClass(String className, boolean... inner)
+    {
+        Class<?> type = null;
+        boolean in = inner.length > 0 ? inner[0] : false;
+        try {
+            if (in)
+            {
+                int last = className.lastIndexOf(".");
+                className = className.substring(0, last) + "$" + className.substring(last+1);
+            }
+            type = Class.forName(className);
+        } catch (ClassNotFoundException e) {}
+        return type;
     }
 }
