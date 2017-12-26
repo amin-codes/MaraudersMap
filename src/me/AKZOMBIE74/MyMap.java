dd73099f7157565b14cb962a016746387efc7d89
AKZOMBIE74 <6474139+AKZOMBIE74@users.noreply.github.com>
2017-12-26 09:29:08 -0500

    Update MyMap.java

--- src/me/AKZOMBIE74/MyMap.java
+++ src/me/AKZOMBIE74/MyMap.java
@@ -16,19 +16,43 @@ public class MyMap implements CommandExecutor {
 
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+        MapView.Scale scale = MM.getInstance().scale();
         if (sender instanceof Player) {
             Player p = (Player) sender;
                 if (p.hasPermission("marauders.use")) {
-                    if (cmd.getName().equalsIgnoreCase("mm")) {
-                        ItemStack is = MM.getInstance().createMap();
-                        MapView mapView = Bukkit.getMap(is.getDurability());
-                        myRender.applyToMap(mapView);
-                        p.getInventory().addItem(is);
-                        p.sendMessage(ChatColor.GREEN + "Received Marauders Map");
+                    if (args.length > 0)
+                    {
+                        p = Bukkit.getPlayer(args[0]);
+                        if (p==null)
+                        {
+                            sender.sendMessage(ChatColor.RED+"Player not found!");
+                            return false;
+                        }
                     }
+                    scale = args.length > 1 ? MapView.Scale.valueOf(args[1]) : scale;
+                    giveMap(p, scale);
+                    if (p!=sender) sender.sendMessage(ChatColor.GREEN+"Successfully gave map to " + p.getName() + "!");
+                    if (args.length > 1 && scale!=MapView.Scale.valueOf(args[1])) sender.sendMessage(ChatColor.RED+"That scale could not be found so the map given has the default scale");
                 }
+        } else {
+            if (args.length > 0)
+            {
+                scale = args.length > 1 ? MapView.Scale.valueOf(args[1]) : scale;
+                giveMap(Bukkit.getPlayer(args[0]), scale);
+                if (args.length > 1 && scale!=MapView.Scale.valueOf(args[1])) sender.sendMessage(ChatColor.RED+"That scale could not be found so the map given has the default scale");
+            }
+            else sender.sendMessage(ChatColor.RED + "Please specify a player!");
         }
         return false;
+    }
+
+    public void giveMap(Player p, MapView.Scale... scale)
+    {
+        MapView.Scale scale1 = scale.length > 0? scale[0] : MM.getInstance().scale();
+        ItemStack is = MM.getInstance().createMap(p.getWorld()).withScale(scale1);
+
+        p.getInventory().addItem(is);
+        p.sendMessage(ChatColor.GREEN + "Received Marauders Map");
     }
 }
